c299e3aa91c8e0c2652e2891578e6229458a2d92
Jason Yang <jasonyang@exosite.com>
2018-11-02 01:53:08 +0800

    Fix server extra chunks content

--- server/renderer.js
+++ server/renderer.js
@@ -70,7 +70,7 @@ export default store => (req, res, next) => {
 
         // map required assets to script tags
         const extraChunks = extractAssets(manifest, modules).map(
-          c => `<script type="text/javascript" src="/${c}"></script>`
+          c => `<script type="text/javascript" src="${c}"></script>`
         );
 
         const helmet = Helmet.renderStatic();
